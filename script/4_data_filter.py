4e847e3b054c6ab4419291f176824a8e7ff724aa
Giovanni Rosa <giovanni.rosa@unimol.it>
2021-12-20 22:45:21 +0100

    bugfix

--- script/4_data_filter.py
+++ script/4_data_filter.py
@@ -17,7 +17,7 @@ regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()
 def detect_lang(comment):
     lang = None
     try:
-        lang = langid.classify(comment[:500])
+        lang = langid.classify(comment)
     except:
         # can not detect language
         logging.warn(traceback.format_exc())
@@ -37,7 +37,7 @@ def main():
                 logging.info('processing row: ' + row['dockerfile_sha1'])
                 comment = row['comment_clean'].lower()
                 words = word_tokenize(comment)
-                if (langid.classify(comment) == 'en' and len(words) >= 2 and comment[0].isalpha()):
+                if (detect_lang(comment) == 'en' and len(words) >= 2 and comment[0].isalpha()):
 
                     url = re.findall(regex, comment)
 
